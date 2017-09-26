ceb33c9f3925adb03a5e6177bb0e7cea3091f988
lavjain <ljain@pivotal.io>
2017-09-25 20:04:29 -0700

    Remove curl from singlecluster

--- singlecluster.bash
+++ singlecluster.bash
@@ -6,7 +6,6 @@ _main() {
   HADOOP_DISTRO_LOWER=$(echo ${2} | tr A-Z a-z)
   mv ${HADOOP_DISTRO_LOWER}_tars_tarball/*.tar.gz $(pwd)/singlecluster/tars/
   mv tomcat/*.tar.gz $(pwd)/singlecluster/tars/
-  mv curl/*.tgz $(pwd)/singlecluster/tars/
   mv jdbc/*.jar $(pwd)/singlecluster/tars/
   pushd $(pwd)/singlecluster
     make HADOOP_VERSION="${1}" HADOOP_DISTRO="${2}"
